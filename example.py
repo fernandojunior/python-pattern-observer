f56db6e91e04011622a2c6cbb175422af98cc0fb
Fernando Felix <fernandojr.ifcg@live.com>
2016-01-11 16:05:05 -0300

    Descrevendo alguns exemplos.

--- example.py
+++ example.py
@@ -22,9 +22,8 @@ from observer import Observable, Event
 # TODO? subject.trigger(event)
 
 
-# Event == Topic
-# Observable == Subject == Source == Event Source == Provider
-# Observer == Listener == Subscriber
+def clicked(a, b, c=None):
+    print('clicked ', a, b, c)
 
 
 def clicked1():
@@ -38,56 +37,114 @@ def clicked2():
 def clicked3():
     print('clicked3.')
 
-
-def mouseentered():
-    print('mouseentered')
-
-
+print('A document event (click) with a handler:')
 document = Observable()
 document.click = Event()
-document.click.on(clicked1)
-document.click.on(clicked2)
-document.click.on(clicked3)
-document.mouseenter = Event(mouseentered)
-document.click.trigger()
-document.mouseenter.trigger()
+document.click.on(clicked)  # add a handler to the event
+document.click.trigger(1, 2, c=3)  # trigger event handler with arguments
 
+print('A document event (click) with a handler:')
 document = Observable()
-document.on('click', [clicked1, clicked2, clicked3])  # multiple handlers
-document.on('mouseenter', mouseentered)
-document.click()
-document.mouseenter()
+document.on('click', clicked)  # create event dynamically
+document.click.trigger(1, 2, c=3)
 
+print('A document event (click) with many handlers:')
+document = Observable()
+document.click = Event()
+document.click.on(clicked1)  # add a handler to the event
+document.click.on(clicked2)  # add another
+document.click.on(clicked3)  # ...
+document.click.trigger()  # trigger all event handlers
+
+print('A document event (click) with many handlers:')
+document = Observable()
+document.on('click', [clicked1, clicked2, clicked3])  # create it dynamically
+document.click.trigger()
+
+print('Three document events each one with its handler:')
 document = Observable()
 document.on({
-    'click': [clicked1, clicked2, clicked3],
-    'mouseenter': mouseentered})
-document.trigger(['click', 'mouseenter'])
+    'click1': clicked1,
+    'click2': clicked2,
+    'click3': clicked3})
+document.click1.trigger()  # different ways to trigger an event
+document.click2()
+document.trigger('click3')
+print('Trigger many events at once:')
+document.trigger(['click1', 'click2', 'click3'])  # trigger many events at once
 
+print('Two document events that contains the same handlers:')
+document = Observable()
+document.on('clicka', [clicked1, clicked2, clicked3])
+document.on('clickb', [clicked1, clicked2, clicked3])
+document.trigger(['clicka', 'clickb'])
+
+print('Two document events that contains the same handlers:')
+document = Observable()
+document.on(['clicka', 'clickb'], [clicked1, clicked2, clicked3])
+document.trigger(['clicka', 'clickb'])
+
+print('Two document events that contains the same handlers:')
+document = Observable()
+document.on('clicka clickb', [clicked1, clicked2, clicked3])
+document.trigger(['clicka', 'clickb'])
+
+
+class ClickEvent(Event):
+
+    def __init__(self):
+        self.on(self.clicked1)
+        self.on(self.clicked2)
+        self.on(self.clicked3)
+
+    def clicked1(self):
+        print('clicked1.')
+
+    def clicked2(self):
+        print('clicked2.')
+
+    def clicked3(self):
+        print('clicked3.')
+
+click_event = ClickEvent()
+click_event2 = ClickEvent()
+
+document = Observable()
+document.on('click', click_event)  # create document event with event instance
+document.on('click', click_event2)  # replace document event behaviour
+document.on('click click_alias', click_event)  # events have the same reference
+
+print('Create many document events with a dictionary:')
+document = Observable()
+document.on({
+    'click click_alias': click_event,
+    'clicka clickb': [clicked1, clicked2, clicked3],
+    'click1': clicked1,
+    'click2': clicked2,
+    'click3': clicked3})
+document.click.trigger()
+document.clicka()
+document.trigger('click1')
+document.trigger(['click2', 'click3'])
+
+
+class Clicked:
+
+    def __init__(self, i):
+        self.msg = 'Clicked{}.'.format(i)
+
+    def __call__(self):
+        print(self.msg)
+
+clicked1 = Clicked(1)
+clicked2 = Clicked(2)
+clicked3 = Clicked(3)
 document = Observable()
 document.on('click', [clicked1, clicked2, clicked3])
-document.on('click2', [clicked1, clicked2, clicked3])  # contains same handlers
-document.on('mouseenter', mouseentered)
-document.trigger(['click', 'mouseenter'])
+document.trigger('click')
 
-document = Observable()
-document.on(['click', 'click2'], [clicked1, clicked2, clicked3])
-document.on('mouseenter', mouseentered)
-document.trigger(['click', 'mouseenter'])
-
-document = Observable()
-document.on('click click2', [clicked1, clicked2, clicked3])
-document.on('mouseenter', mouseentered)
-document.trigger(['click', 'mouseenter'])
-
-document = Observable()
-document.on({
-    'click click2': [clicked1, clicked2, clicked3],
-    'mouseenter': mouseentered})
-document.trigger(['click', 'mouseenter'])
 
 # TODO? document.trigger('click mouseenter')
-
 # TODO? descritor para adicionar a test a w.events automaticamente
 
 
